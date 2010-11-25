4632c4df954fb3a06c50391c7383e0415ae147a8
Christopher Thomas <cwt137@gmail.com>
2010-11-25 05:53:24 -0800

    changed the name so there are no clashes

---
+++ libs/geo_inflector.php
@@ -0,0 +1,39 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Inflector
+ *
+ * @author cwthomas
+ */
+class InflectorComponent {
+
+    public static function titleize($word) {
+
+    }
+    
+    public static function camelize($str) {
+
+    }
+
+    public static function humanize($lowerCaseAndUnderscoredWord) {
+
+    }
+
+    public static function snakeCase($s) {
+
+    }
+
+    public static function underscore($camelCasedWord) {
+
+    }
+
+    public static function urlEscape($s) {
+
+    }
+
+}
+?>
